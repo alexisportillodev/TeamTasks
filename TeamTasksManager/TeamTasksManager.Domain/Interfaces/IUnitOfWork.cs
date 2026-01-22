e663a7fca78ffc5530be11ac0ef73934864544f0
alexisportillo-admin <alexisportillo95@outlook.com>
2026-01-22 00:12:58 -0500

    feat(domain): crear interface IUnitOfWork para transacciones

---
+++ TeamTasksManager/TeamTasksManager.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using TeamTasksManager.Domain.Interfaces.Repositories;
+
+namespace TeamTasksManager.Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IDeveloperRepository Developers { get; }
+        IProjectRepository Projects { get; }
+        ITaskRepository Tasks { get; }
+
+        Task SaveChangesAsync();
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+    }
+}
