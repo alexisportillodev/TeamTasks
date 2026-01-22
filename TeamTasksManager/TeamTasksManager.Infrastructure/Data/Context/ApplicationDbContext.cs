04335210040c25a02ef4be513a8af7283d931c20
alexisportillo-admin <alexisportillo95@outlook.com>
2026-01-22 00:27:45 -0500

    feat(infrastructure): crear ApplicationDbContext con DbSets

---
+++ TeamTasksManager/TeamTasksManager.Infrastructure/Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeamTasksManager.Domain.Entities;
+using TeamTasksManager.Infrastructure.Data.Configurations;
+
+namespace TeamTasksManager.Infrastructure.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Developer> Developers { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<TaskItem> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Aplicar configuraciones
+            modelBuilder.ApplyConfiguration(new DeveloperConfiguration());
+            modelBuilder.ApplyConfiguration(new ProjectConfiguration());
+            modelBuilder.ApplyConfiguration(new TaskItemConfiguration());
+        }
+    }
+}
