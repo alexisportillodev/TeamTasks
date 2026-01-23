57c16b03d28aab2fb444a82ad5362b3490adedea
alexisportillo-admin <alexisportillo95@outlook.com>
2026-01-23 00:45:08 -0500

    feat(frontend): configurar ruta para formulario de nueva tarea

--- team-tasks-dashboard/src/app/app.routes.ts
+++ team-tasks-dashboard/src/app/app.routes.ts
@@ -11,26 +11,16 @@ export const routes: Routes = [
     loadComponent: () => import('./features/dashboard/dashboard.component')
       .then(m => m.DashboardComponent)
   },
-  // {
-  //   path: 'projects', // lista de proyectos
-  //   loadComponent: () => import('./features/projects/components/project-list/project-list.component')
-  //     .then(m => m.ProjectListComponent)
-  // },
   {
-    path: 'projects/:id/tasks', // tareas de un proyecto
+    path: 'projects/:id/tasks',
     loadComponent: () => import('./features/projects/components/project-tasks/project-tasks.component')
       .then(m => m.ProjectTasksComponent)
   },
-  // {
-  //   path: 'tasks', // tareas generales
-  //   loadComponent: () => import('./features/tasks/components/task-list/task-list.component')
-  //     .then(m => m.TaskListComponent)
-  // },
-  // {
-  //   path: 'developers', // lista de desarrolladores
-  //   loadComponent: () => import('./features/developers/components/developer-list/developer-list.component')
-  //     .then(m => m.DeveloperListComponent)
-  // },
+  {
+    path: 'tasks/new',
+    loadComponent: () => import('./features/tasks/components/task-form/task-form.component')
+      .then(m => m.TaskFormComponent)
+  },
   {
     path: '**',
     redirectTo: 'dashboard'
